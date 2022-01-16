9da29b72f8842bf901c920d13f929345db84f2cf
Piyotr-K <piyotrkao1998@outlook.com>
2022-01-15 22:16:42 -0800

    Master password spliting part 1
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- Encrypter.py
+++ Encrypter.py
@@ -10,7 +10,7 @@ Maybe static?
 
 Author: Piyotr Kao
 Date-Created: 2021 NOV 09
-Date-Modified: 2022 JAN 10
+Date-Modified: 2022 JAN 15
 """
 class Encrypter:
 
@@ -21,7 +21,7 @@ class Encrypter:
         self._encoding = "utf-8"
         self._state = 0
         # Check for the master password first
-        self.master_pass()
+        self.master_pass_check()
 
         # If they haven't entered the correct password
         if self._state == -1:
@@ -49,24 +49,27 @@ class Encrypter:
             # setup fernet object
             self._fernet = Fernet(self._key)
 
-    def master_pass(self):
+    def master_pass_check(self):
+        """
+        Attempts to read for a master password, if not found, means that its the first time
+        running the program, will ask user to create a new master password
+
+        For the final version need to delete all files when generating a new password
+        """
         try:
             self._master = self.read_master()
 
             while True:
                 ui = input("Enter your master password: ")
 
-                m = hashlib.sha256()
-                m.update(bytes(ui, self._encoding))
-                tmp = m.digest()
+                tmp = self.gen_hash(ui)
                 if tmp != self._master:
                     print("Wrong pass word, try again")
                     self._state = -1
                 else:
-                    print("vvelcommen")
+                    print("Welcome")
                     self._state = 0
                     break
-
         except FileNotFoundError:
             # If there is no master password yet
             print("Master password not found ... Generating New Master Password")
@@ -79,10 +82,12 @@ class Encrypter:
                 else:
                     break
             
-            m = hashlib.sha256()
-            m.update(bytes(ui, self._encoding))
-            self.write_master(m.digest())
+            self.write_master(self.gen_hash(ui))
 
+    def gen_hash(self, data: str) -> bytes:
+        m = hashlib.sha256()
+        m.update(bytes(data, self._encoding))
+        return m.digest()
     
     def read_key(self):
         # get key from file
